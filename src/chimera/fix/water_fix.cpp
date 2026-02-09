dea7bc085532dbf40f0cf22588bcd744607f05d6
lfsky <81259956+lfsky@users.noreply.github.com>
2026-02-08 22:19:28 -0600

    Fad

--- src/chimera/fix/water_fix.cpp
+++ src/chimera/fix/water_fix.cpp
@@ -33,11 +33,35 @@ namespace Chimera {
 
     extern "C" void set_water_shader_const(std::byte *shader, std::uint32_t start_register) noexcept {
         // Not how it works on real hardware NV2A, Collection users seem to have an issue with this.
-		// Rounded off is closer than plastic looking dogwater shoved down an end users throat.
+		// Rounded off is closer than plastic looking dogwater shoved down unsuspecting throats.
+		// Formula is to 1080x810ify the increase, more or less accurate across resolutions.
+		// To do this, we need to artifically increase the dimensions of xy in our math.
+		// Include width, otherwise is automatically determined. Account for aspect ratio.
+		// Required, since otherwise at lower than this the divison ends up less than 1.
+		// Results in shifting the appearance which happens as the display changes.
+		// Fov is also taken to account on those, in which 55.41v used as reference.
+		// Specific integer is closely around 3840/810 = x*y = 4.740740740740741
+		// Instead of directly putting log2f(4.740740740740741) which also works.
+		// 2.481481481481481 × 2.037037037037037  (2x) = 5.054869684499313.
+		// 1651 / 1238  = 1.333333333 1650 as close as we can get using rounding.
+		// 2.396769969445657 × 1.977881257275902 (1.89x) = 4.740526400568301
+		// Let the engine do the math, and change the numbers as desired for whatever.
+		// Consider whether or not the engine itself genuinely does this. Convert it.
+		// 1294 / 1934 using the largest uniform value for the width so it's covered.
+		// 2.654601861427094 × 1.78593588417787 (1.89x) = 4.740948722528016
+		// Identify whether which number is the culprit for the shifting lod line.
+		// Revert to how we did it modified since we know this will defintively work.
+		// 1.832920178482895 × 2.586514625681706 (1.89x) = 4.740874849353132
+		// At UHD looks 100% authentic to 1080x810 or real hardware using SD.
+		// Lower than 1080x810 reverts to 1 size, will investigate later.
+		// log2f((get_resolution().height + 1375) / 2015.f) * log2f((get_resolution().width + 1535) / 2015.f) initial commit
+		// log2f((get_resolution().width + 1375) / 2015.f) * log2f((get_resolution().height + 1535) / 2015.f) rev1
+		// log2f((get_resolution().width + 1651) / 2291.f) * log2f((get_resolution().height + 1238) / 1718.f) aspect ratio
+		// log2f((get_resolution().width + 1294) / 1934.f) * log2f((get_resolution().height + 1454) / 1934.f) rev3
         if(d3d9_device_caps->PixelShaderVersion >= 0xffff0101 && start_register == 1) {
 
             // Why would you do that. Why would you do that.
-            float adjusted_lod_bias = log2f((get_resolution().width + 1375) / 2015.f) * log2f((get_resolution().height + 1535) / 2015.f) - (*reinterpret_cast<float *>(shader + 0xE0));
+            float adjusted_lod_bias = log2f((get_resolution().width + 1377) / 2017.f) * log2f((get_resolution().height + 1537) / 2017.f) - (*reinterpret_cast<float *>(shader + 0xE0));
 
             // Bump map is on sampler 0.
             DWORD *mip_lod_bias = reinterpret_cast<DWORD *>(&adjusted_lod_bias);
